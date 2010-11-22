c185545d6d11fa0bf87606a49c0d744463fc3062
ValerianCucularu <vallerry_incense@yahoo.com>
2010-11-22 18:29:06 +0200

    added clear functionality

--- cli.js
+++ cli.js
@@ -15,6 +15,45 @@ return code;
 }
 
 /**
+	Function for handling printable characters & other special characters
+	**/
+function init_keyboard(e) {
+	
+	var code = get_key(e);
+
+	if ((32 <= code) && (code <= 126))
+		handler.writeCharacter(code);
+
+	else if (code == 13)
+		handler.carriageReturn();
+
+	else if (code == 8)
+		handler.backSpace();
+}
+
+/**
+	Function for checking if a valid command has been typed
+	**/
+
+function valid_command() {
+
+	var typed = handler.consoleLine.previousSibling.innerHTML.substring(handler.consoleHead.length),
+			flag = false;
+	for (var i in commands) {
+		if (commands[i].text == typed.toLowerCase()) {
+			commands[i].action();
+			flag = true;
+			break;
+		}
+	}
+	if (!flag) {
+		var err = document.createElement("p");
+		err.innerHTML = "Command not found";
+		handler.consoleLine.parentNode.insertBefore(err, handler.consoleLine);
+	}
+}
+
+/**
 	Series of functions for handling keyboard events according to the keys pressed
 	**/
 
@@ -22,11 +61,12 @@ var handler = {
 
 	consoleHead : "root@cucuX:~#",
 
+	consoleID : 'console',
+
 	consoleLine : null,
 
-	setConsoleLine : function(ref) {
-	
-		this.consoleLine = ref;
+	setConsoleLine :  function() {
+		this.consoleLine = document.getElementById(this.consoleID);
 	},
 
 	carriageReturn : function() {
@@ -35,6 +75,7 @@ var handler = {
 		par.innerHTML = this.consoleLine.innerHTML;
 		this.consoleLine.parentNode.insertBefore(par, this.consoleLine);
 		this.consoleLine.innerHTML = this.consoleHead;
+		valid_command();
 	},
 
 	backSpace : function() {
@@ -52,19 +93,22 @@ var handler = {
 };
 
 /**
-	Function for handling printable characters
+	List of supported commands by the console
 	**/
-function init_keyboard(e) {
 
-	var code = get_key(e);
+var commands = {
 
-	if ((32 <= code) && (code <= 126))
-		handler.writeCharacter(code);
+		"clear" : {
 
-	else if (code == 13)
-		handler.carriageReturn();
+			text : "clear",
 
-	else if (code == 8)
-		handler.backSpace();
-}
+			action : function () {
+				var txt = "<p id='" + handler.consoleLine.id + "'>" + handler.consoleHead + "</p>";
+				handler.consoleLine.parentNode.innerHTML = txt;
+				handler.setConsoleLine();
+			}
+		}	
+};
+
+
 
